9fe2e22ae14048dbc2620c886f6a5ab715b59581
Cameron-Lyons <cameron.lyons2@gmail.com>
2021-12-19 18:13:50 -0600

    saved jpgs to seperate folders

--- scripts/prep.py
+++ scripts/prep.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     prior to use in model
     """
 
-    df = pd.read_csv("../outputs/final_df.csv", low_memory=False)
+    df = pd.read_csv("./outputs/final_df.csv", low_memory=False)
 
     df['calendar_updated'] = pd.to_datetime(df['calendar_updated'])
     df['first_review'] = pd.to_datetime(df['first_review'], errors='coerce')
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     X = df.drop(['price', 'id', 'listing_url', 'scrape_id',
                 'last_scraped',
-                'picture_url', 'host_id', 'host_url', 'host_name',
+                'host_id', 'host_url', 'host_name',
                 'host_thumbnail_url', 'host_picture_url', 'calendar_last_scraped',
                 ], axis=1)
 
@@ -43,18 +43,43 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     X_train, X_dev, y_train, y_dev = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
-
-    X_train.to_csv("../outputs/X_train.csv", index=False)
-    X_dev.to_csv("../outputs/X_dev.csv", index=False)
-    X_test.to_csv("../outputs/X_test.csv", index=False)
-
-    y_train.to_csv("../outputs/y_train.csv", index=False)
-    y_dev.to_csv("../outputs/y_dev.csv", index=False)
-    y_test.to_csv("../outputs/y_test.csv", index=False)
-
+    
     for i, image_url in X_train["picture_url"].iteritems():
         r = requests.get(image_url, stream=True)
         if r.status_code == 200:
-            with open('../outputs/images/' + i, 'wb') as f:
+            with open('./outputs/images/train/' + str(i) +'.jpg', 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+    
+    for i, image_url in X_dev["picture_url"].iteritems():
+        r = requests.get(image_url, stream=True)
+        if r.status_code == 200:
+            with open('./outputs/images/dev/' + str(i) +'.jpg', 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+    
+    for i, image_url in X_test["picture_url"].iteritems():
+        r = requests.get(image_url, stream=True)
+        if r.status_code == 200:
+            with open('./outputs/images/test' + str(i) +'.jpg', 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f) 
+
+    X_train.drop(['picture_url'], axis=1, inplace=True)
+    X_dev.drop(['picture_url'], axis=1, inplace=True)
+    X_test.drop(['picture_url'], axis=1, inplace=True)
+
+    X_train.to_csv("./outputs/X_train.csv", index=False)
+    X_dev.to_csv("./outputs/X_dev.csv", index=False)
+    X_test.to_csv("./outputs/X_test.csv", index=False)
+
+    y_train.to_csv("./outputs/y_train.csv", index=False)
+    y_dev.to_csv("./outputs/y_dev.csv", index=False)
+    y_test.to_csv("./outputs/y_test.csv", index=False)
+    print(X_test.head())
+    for i, image_url in X_train["picture_url"].iteritems():
+        r = requests.get(image_url, stream=True)
+        if r.status_code == 200:
+            with open('./outputs/images/' + i, 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
